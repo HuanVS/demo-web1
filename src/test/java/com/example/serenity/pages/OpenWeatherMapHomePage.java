3e3c21a1c13c6d04af71b1585382422da2707ea7
Vuong Sy Huan <v.s.huanbk@gmail.com>
2025-03-07 23:37:58 +0700

    update xpath

--- src/test/java/com/example/serenity/pages/OpenWeatherMapHomePage.java
+++ src/test/java/com/example/serenity/pages/OpenWeatherMapHomePage.java
@@ -1,14 +1,15 @@
 package com.example.serenity.pages;
 
 import net.serenitybdd.core.annotations.findby.FindBy;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class OpenWeatherMapHomePage extends BasePage {
 
-    @FindBy(xpath = "//div[@id='desktop-menu']//input[@placeholder='Search city']")
+    @FindBy(xpath = "//input[@placeholder='Search city']")
     private WebElement searchInput;
 
-    @FindBy(xpath = "//div[@id='desktop-menu']//button[contains(text(),'Search')]")
+    @FindBy(xpath = "//button[contains(text(),'Search')]")
     private WebElement searchButton;
 
     @FindBy(xpath = "//div[contains(@class,'current-container')]//h2")
@@ -17,15 +18,17 @@ public class OpenWeatherMapHomePage extends BasePage {
     // open OpenWeatherMap
     public void openHomePage() {
         openUrl("https://openweathermap.org/");
+        getDriver().manage().window().maximize();
     }
 
     // search City
     public void searchCity(String city) {
         try {
             waitForVisibility(searchInput, 10);
-            searchInput.clear();
             enterText(searchInput, city);
             clickElement(searchButton);
+            String dynamicXpath = String.format("//span[contains(text(),'%s')]/..", city);
+            getDriver().findElement(By.xpath(dynamicXpath)).click();
         } catch (Exception e) {
             System.out.println("Error in searchCity: " + e.getMessage());
             throw e;
